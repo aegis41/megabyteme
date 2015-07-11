1319edf642ad73e97601e26678b202344f7965dd
aegis41 <garrrocks@gmail.com>
2015-07-10 20:34:26 -0400

    ticker, upgrades, and display
    Ticker is working, Components can upgrade, and display upgrades
    appropriately. Lots more work to do.

--- js/megabyteme.js
+++ js/megabyteme.js
@@ -1,5 +1,5 @@
 // the timer to run code every second
-var Time = window.setInterval(function(){Tick()},1000);
+var Timer = window.setInterval(function(){Tick()},1000);
 
 // the array of components
 var Components = [];
@@ -7,7 +7,7 @@ var Components = [];
 // the object constructor for save games
 function GameSave()
 {
-  this.bytes = 0;
+  this.bytes = 10;
 }
 
 //the object model for components
@@ -19,38 +19,63 @@ function Component(name, cost, persec, level)
   this.level = level;
 }
 
+
 // the function to initialize the components
 function InitComponents()
 {
-  Components[0] = new Component("CPU",10,1,1);
-  Components[1] = new Component("MOBO",50,5,1);
-  Components[2] = new Component("RAM",1000,10,1);
+  Components[0] = new Component("CPU",10,1,0);
+  Components[1] = new Component("MOBO",50,5,0);
+  Components[2] = new Component("RAM",1000,10,0);
 }
+
+// the function to initialize the display
+function InitDisplay()
+{
+	for (var i = 0; i < Components.length; i++)
+	{
+		var idname = Components[i].name.toLowerCase();
+		var lvl = idname + "-level";
+		var bps = idname + "-bps";
+		var btn = idname + "-btn";
+    document.getElementById(lvl).innerHTML = Components[i].level;
+    document.getElementById(bps).innerHTML = Components[i].persec;
+		document.getElementById(btn).value = Components[i].name + " (" + Components[i].cost + ")";
+	}
+}
+
 
 // the function that runs at the interval
 function Tick()
 {
-  for(  var i = 0; i < Components.length; i++)
+	game.bytes++;
+  for(var i = 0; i < Components.length; i++)
   {
-    game.bytes += 1 + (Components[i].level * Components[i].persec);
+    game.bytes += (Components[i].level * Components[i].persec);
   }
-  document.getElementByID("bytes").innerHTML = game.bytes;
+  document.getElementById("total-bytes").innerHTML = game.bytes;
 }
 
 function Upgrade(id)
 {
   if (game.bytes >= Components[id].cost) // is there enough money?
   {
-    game.bytes -= Components[i].cost; // decrease the money by the cost
+    game.bytes -= Components[id].cost; // decrease the money by the cost
     Components[id].level++; //increase the component level
-    Components[id].cost = Components[id] * 1.25;
-    document.getElementByID(Components[id].name.toLowerCase() + "-level").innerHTML = Component[id].level;
-    document.getElementByID(Components[id].name.toLowerCase() + "-bps").innerHTML = Component[id].persec;
-  }
+    Components[id].cost = Math.round(Components[id].cost * 1.25);
+		var idname = Components[id].name.toLowerCase();
+		var lvl = idname + "-level";
+		var bps = idname + "-bps";
+		var btn = idname + "-btn";
+		document.getElementById("total-bytes").innerHTML = game.bytes;
+    document.getElementById(lvl).innerHTML = Components[id].level;
+    document.getElementById(bps).innerHTML = Components[id].persec;
+		document.getElementById(btn).value = Components[id].name + " (" + Components[id].cost + ")";
+	}
 }
 
-window.onLoad = Function()
+window.onload = function()
 {
   InitComponents();
+	InitDisplay();
   window.game = new GameSave();
 }
