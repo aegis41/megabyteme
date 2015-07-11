f9fe40465ed48cad6a00bba68abdbd7e43ad9124
aegis41 <garrrocks@gmail.com>
2015-07-11 01:05:04 -0400

    Warriors, Sacrificed, Firewall, Mal Softs
    Added warriors, sacrificed properties, adjusted some styling, added mal
    soft object model.

--- js/megabyteme.js
+++ js/megabyteme.js
@@ -4,32 +4,59 @@ var Timer = window.setInterval(function(){Tick()},1000);
 // the array of components
 var Components = [];
 
+// the array of malicious software
+var Baddies = [];
+
 // the object constructor for save games
 function GameSave()
 {
-  this.bytes = 10;
+  this.bytes = 0;					// the current level of bytes
+	this.bps = 1;						// the current level of bytes per second
+	this.baddielvl = 1;			// the baddies' level
+	this.warriors = 0;			// the current level of warriors
+	this.sacrificed = 0;		// the current level of sacrificed
 }
 
 //the object model for components
-function Component(name, cost, persec, level)
+function Component(name, cost, persec, level,text)
 {
   this.name = name;
   this.cost = cost;
   this.persec = persec;
   this.level = level;
+	this.text = text;
 }
 
+// the object model for the baddies
+function Baddie(type, power, reward)
+{
+	this.type = type;
+	this.power = power;
+	this.reward = reward;
+}
+
+// the function to make a new baddie
+function MakeBaddie()
+{
+	if (Baddies.lenth < 5)
+	{
+		
+	}
+	
+}
 
 // the function to initialize the components
 function InitComponents()
 {
-  Components[0] = new Component("CPU",10,1,0);
-  Components[1] = new Component("MOBO",50,5,0);
-  Components[2] = new Component("RAM",1000,10,0);
+  Components[0] = new Component("CPU",10,1,0,"CPU");
+  Components[1] = new Component("MOBO",50,5,0,"MoBo");
+  Components[2] = new Component("RAM",1000,10,0,"RAM");
+	Components[3] = new Component("AV",100,10,0,"Anti-Virus");
+	Components[4] = new Component("MW",100,10,0,"Malware Agent");
 }
 
 // the function to initialize the display
-function InitDisplay()
+function UpdateDisplay()
 {
 	for (var i = 0; i < Components.length; i++)
 	{
@@ -39,20 +66,22 @@ function InitDisplay()
 		var btn = idname + "-btn";
     document.getElementById(lvl).innerHTML = Components[i].level;
     document.getElementById(bps).innerHTML = Components[i].persec;
-		document.getElementById(btn).value = Components[i].name + " (" + Components[i].cost + ")";
+		document.getElementById(btn).value = Components[i].text + " (" + Components[i].cost + ")";
+		document.getElementById("bytes-per-sec").innerHTML = game.bps;
 	}
+	document.getElementById("total-bytes").innerHTML = game.bytes;
+	document.getElementById("warrior-bytes").innerHTML = game.warriors;
 }
 
 
 // the function that runs at the interval
 function Tick()
 {
-	game.bytes++;
-  for(var i = 0; i < Components.length; i++)
-  {
-    game.bytes += (Components[i].level * Components[i].persec);
-  }
-  document.getElementById("total-bytes").innerHTML = game.bytes;
+
+	game.bps = BytesPerSecond();
+	game.bytes += game.bps;
+	game.warriors += game.bps;
+	UpdateDisplay();
 }
 
 function Upgrade(id)
@@ -62,20 +91,24 @@ function Upgrade(id)
     game.bytes -= Components[id].cost; // decrease the money by the cost
     Components[id].level++; //increase the component level
     Components[id].cost = Math.round(Components[id].cost * 1.25);
-		var idname = Components[id].name.toLowerCase();
-		var lvl = idname + "-level";
-		var bps = idname + "-bps";
-		var btn = idname + "-btn";
-		document.getElementById("total-bytes").innerHTML = game.bytes;
-    document.getElementById(lvl).innerHTML = Components[id].level;
-    document.getElementById(bps).innerHTML = Components[id].persec;
-		document.getElementById(btn).value = Components[id].name + " (" + Components[id].cost + ")";
+		game.bps = BytesPerSecond();
+		UpdateDisplay();
 	}
+}
+
+function BytesPerSecond()
+{
+	var bps = 1;
+  for(var i = 0; i < Components.length; i++)
+  {
+    bps += (Components[i].level * Components[i].persec);
+  }
+	return bps;
 }
 
 window.onload = function()
 {
-  InitComponents();
-	InitDisplay();
   window.game = new GameSave();
+  InitComponents();
+	UpdateDisplay();
 }
